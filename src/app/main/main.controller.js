53127f8ac7e3d6bafd2bdaf42ce8a02df31df63c
Ivan Dalmet <ivan@creativedata.fr>
2016-05-26 18:13:39 +0200

    First commit

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+    // var vm = this;
+    
+    activate();
+
+    function activate() {
+    }
+  }
+})();
