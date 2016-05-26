39bba99b981c440e20d3cfe47b413c79961fef9f
Ivan Dalmet <ivan@creativedata.fr>
2016-05-26 18:53:12 +0200

    Add font-awesome.

--- gulp/build.js
+++ gulp/build.js
@@ -51,6 +51,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(cssFilter)
     // .pipe($.sourcemaps.init())
     .pipe($.cssnano())
+    .pipe($.replace('../../bower_components/font-awesome/fonts', '../fonts'))
     .pipe($.rev())
     // .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
@@ -65,7 +66,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(htmlFilter.restore)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
     .pipe($.size({ title: path.join(conf.paths.dist, '/'), showFiles: true }));
-  });
+});
 
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
