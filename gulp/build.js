58e0650a0050b6c6ae728db738adddeb401b1fbe
Ivan Dalmet <ivan@creativedata.fr>
2016-06-30 17:24:50 +0200

    fix(gulp): Remove duplicate customs fonts output in dist folder.

--- gulp/build.js
+++ gulp/build.js
@@ -75,7 +75,6 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
   var files = $.mainBowerFiles();
-  files.push('src/assets/fonts/**/*');
 
   return gulp.src(files)
     .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
